4b9fd9c2ae32ba6e69470bce0caf67c4cef91b70
tobiasschulz1 <tobiasschulz.2000.ts@gmail.com>
2025-09-12 19:27:21 +0200

    created an onboarding process

---
+++ littlelemon/littlelemon/Menu.swift
@@ -0,0 +1,25 @@
+//
+//  Menu.swift
+//  littlelemon
+//
+//  Created by Tobias Schulz on 12.09.25.
+//
+
+import SwiftUI
+
+struct Menu: View {
+    var body: some View {
+        VStack {
+            Text("Little Lemon")
+            Text("Chicago")
+            Text("This app lets user order their favorite food from a menu")
+            List {
+
+            }
+        }
+    }
+}
+
+#Preview {
+    Menu()
+}
