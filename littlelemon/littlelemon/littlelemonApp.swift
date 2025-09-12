177422b8738a13edf2555398cabf4b25a6b0462b
tobiasschulz1 <tobiasschulz.2000.ts@gmail.com>
2025-09-12 11:16:17 +0200

    Initial setup

---
+++ littlelemon/littlelemon/littlelemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  littlelemonApp.swift
+//  littlelemon
+//
+//  Created by Tobias Schulz on 12.09.25.
+//
+
+import SwiftUI
+
+@main
+struct littlelemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
